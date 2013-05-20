9e9c6d878f6e2a5727a75dc1f39d4f938a7ed72c
Simon Jagoe <sjagoe@enthought.com>
2013-05-20 11:42:44 +0100

    Added grin to disorganized-pack for fast access before I make a specialized pack

--- init.el
+++ init.el
@@ -2,10 +2,13 @@
 (live-add-pack-lib "sj-ediff")
 (live-add-pack-lib "sj-misc-custom")
 (live-add-pack-lib "sj-linux")
+(live-add-pack-lib "grin")
 
 (require 'sj-ediff)
 (require 'sj-misc-custom)
 (require 'sj-linux)
+(require 'grin)
+
 (define-coding-system-alias 'UTF-8 'utf-8)
 (setq-default display-buffer-reuse-frames t)
 (setq line-move-visual nil)
